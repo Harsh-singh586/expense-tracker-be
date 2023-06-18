1ae01b0a896c65b49f0f0c3df486544d90c46f02
Harsh kumar Singh <singhharshhs586@gmail.com>
2023-06-18 13:21:52 +0530

    exposing endpoint

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+
+// models import
+const Expense = require('./model/expense')
+const Category = require('./model/category')
+const Income = require('./model/income')
+const Budget = require('./model/budget')
+
+//utils import
+const { test } = require('./utils')
+const verify = require('./auth')
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+const PORT = 3001;
+
+const { commonFunction } = require('./commonmodules');
+
+//------------------------------------ LOGIN ----------------------------------
+
+
+app.post('/login', function (req, res) {
+    verify(req, res)
+})
+
+
+//-----------------------------------  Routes -----------------------------------------
+app.all('/expense/:id?', test, function (req, res) {
+    commonFunction(req, res, Expense)
+})
+
+app.all('/category/:id?', test, function (req, res) {
+    commonFunction(req, res, Category)
+})
+
+app.all('/income/:id?', test, function (req, res) {
+    commonFunction(req, res, Income)
+})
+
+app.all('/budget/:id?', test, function (req, res) {
+    commonFunction(req, res, Budget)
+})
+//---------------------------------  Mongo DB Connection-------------------------------
+
+mongoose.connect('mongodb://root:rootpassword@localhost:27017/testlearn')
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+//----------------------------------------------------------------------------------------------
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log("Server is Successfully Running, and App is listening on port " + PORT)
+    else
+        console.log("Error occurred, server can't start", error);
+}
+);
